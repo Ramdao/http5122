a869519c894ef4e27ed76d46c7f32b5aaa0741ed
Tashrif Radin Ali <radin00@hotmail.com>
2024-09-10 19:04:57 -0400

    added weeks

---
+++ week1/JS-LAB-1-IntroToLogic-24f/lab1.js
@@ -0,0 +1,8 @@
+//alert("Hello world");
+var Name = "Tashrif Radin Ali"; //String
+var Age = 24; //Number
+var NumOfDayYear = 365;
+var Totalage = Age * NumOfDayYear;
+var text = "days old... more or less";
+//alert(Totalage);
+alert(Name+ " is "+ Totalage +" old...more or less");
