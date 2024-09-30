58459730cfc362392ea40044f60853403f17183c
Tashrif Radin Ali <radin00@hotmail.com>
2024-09-30 17:02:54 -0400

    week5

---
+++ week5/JS-LAB-4-Functions-24f/lab-4-2.js
@@ -0,0 +1,29 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 2:  AN AVERAGE FUNCTION
+
+
+//################## CREATE YOUR AVERAGE FUNCTION
+//This function takes five numbers and returns their average to one decimal place.
+function avg1(num1,num2,num3,num4,num5){
+    var total=  num1+num2+num3+num4+num5;
+    var avg = total/5.0
+    return avg;
+}
+
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+
+console.log(avg1(5,10,15,20,25).toFixed(1));
+
+var grade1 = parseInt(prompt("grade 1"))
+var grade2 = parseInt(prompt("grade 2"))
+var grade3 = parseInt(prompt("grade 3"))
+var grade4 = parseInt(prompt("grade 4"))
+var grade5 = parseInt(prompt("grade 5"))
+
+if(avg1(grade1,grade2,grade3,grade4,grade5)>=70){
+    console.log(avg(grade1,grade2,grade3,grade4,grade5))
+    alert("Avg 70 or over")
+} else {
+    alert("Review required")
+}
---
+++ week5/JS-LAB-4-Functions-24f/lab-4-3.js
@@ -0,0 +1,26 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 3:  SAFE DOG WALKING CHECK 
+
+
+//================== CREATE YOUR checkTemp FUNCTION
+//This function's job is to... Check to see if it is good for dog walking
+//It needs to receive... a number
+//It will return... boolean= true or false 
+
+function checkTemp(currentTemp){
+    if (currentTemp>30 || currentTemp<-10){
+        return false;
+    }
+    return true;
+}
+
+//================== LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+
+var temp= prompt("current temp");
+temp= checkTemp(temp);
+if(temp===false){
+    alert("Yikes! not good for dog walking!");
+} else if (temp===true){
+    alert("You're good, have a nice walk!")
+}
+
